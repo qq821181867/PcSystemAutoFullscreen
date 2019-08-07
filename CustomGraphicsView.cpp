56f393432ce0cda7ccbde875c9fc75e12166633f
821181867@qq.com <821181867@qq.com>
2019-08-07 16:00:21 +0800

    Signed-off-by: 821181867@qq.com <821181867@qq.com>

---
+++ CustomGraphicsView.cpp
@@ -0,0 +1,93 @@
+﻿#include "CustomGraphicsView.h"
+#include "View/MainWindow/MainWindow.h"
+#include <QGraphicsProxyWidget>
+#include <QDebug>
+#include <QApplication>
+#include <QDesktopWidget>
+
+QSize CustomGraphicsView::getNormalSize()
+{
+    return QSize(1920,1080);
+}
+
+CustomGraphicsView::CustomGraphicsView(QWidget *parent) : QGraphicsView(parent)
+{
+    setFrameShape(QFrame::NoFrame);
+    setLineWidth(0);
+    setRenderHint(QPainter::Antialiasing);
+    setRenderHint(QPainter::SmoothPixmapTransform);
+}
+
+CustomGraphicsView::CustomGraphicsView(QGraphicsScene *scene, QWidget *parent): QGraphicsView(scene, parent)
+{
+    setFrameShape(QFrame::NoFrame);
+    setLineWidth(0);
+    setRenderHint(QPainter::Antialiasing);
+    setRenderHint(QPainter::SmoothPixmapTransform);
+}
+
+void CustomGraphicsView::setMainWidget(QWidget *pWidget)
+{
+    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    if(scene())
+    {
+        pWidget->installEventFilter(this);
+        m_pMainWindow = pWidget;
+        connect(pWidget,SIGNAL(destroyed(QObject*)),this,SLOT(deleteLater()));
+        //
+        this->setWindowFlags(pWidget->windowFlags());
+        QGraphicsProxyWidget *pProxyWidget = scene()->addWidget(pWidget,pWidget->windowFlags());
+        connect(pProxyWidget,&QGraphicsProxyWidget::geometryChanged,this,&CustomGraphicsView::slotGeometryChanged);
+
+        connect(pProxyWidget,&QGraphicsProxyWidget::visibleChanged,this,[this,pWidget]{
+            if(pWidget->windowState()== (Qt::WindowMinimized|Qt::WindowFullScreen))
+            {
+                this->showMinimized();
+                return;
+            }
+            setVisible(pWidget->isVisible());
+        });
+    }
+}
+
+bool CustomGraphicsView::event(QEvent *event)
+{
+    bool isState = QGraphicsView::event(event);
+    if(event->type()==QEvent::WindowStateChange)
+    {
+        if(this->windowState()!=(Qt::WindowMinimized|Qt::WindowFullScreen)&&m_pMainWindow)
+        {
+            m_pMainWindow->setWindowState(this->windowState());
+        }
+    }
+    return isState;
+}
+
+bool CustomGraphicsView::eventFilter(QObject *obj, QEvent *event)
+{
+    bool result = QGraphicsView::eventFilter(obj,event);
+    if(m_pMainWindow==obj)
+    {
+        if(event->type()==QEvent::Show)
+        {
+            this->show();
+        }
+        else if(event->type()==QEvent::Close)
+        {
+            exit(0);
+        }
+    }
+    return result;
+}
+
+void CustomGraphicsView::slotGeometryChanged()
+{
+    QSize sizeNormal = CustomGraphicsView::getNormalSize();
+    if(m_pMainWindow->size()!=sizeNormal)
+    {
+        m_pMainWindow->setGeometry(0,0,sizeNormal.width(),sizeNormal.height());
+        //
+        this->scale(double(this->width())/sizeNormal.width(),double(this->height())/sizeNormal.height());
+    }
+}
---
+++ CustomGraphicsView.h
@@ -0,0 +1,31 @@
+﻿#ifndef CUSTOMGRAPHICSVIEW_H
+#define CUSTOMGRAPHICSVIEW_H
+
+#include <QGraphicsView>
+
+class MainWindow;
+class CustomGraphicsView : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    static QSize getNormalSize();
+public:
+    CustomGraphicsView(QWidget *parent = Q_NULLPTR);
+    CustomGraphicsView(QGraphicsScene *scene, QWidget *parent = Q_NULLPTR);
+    //
+    void setMainWidget(QWidget *pWidget);
+
+protected:
+    bool event(QEvent *event);
+    bool eventFilter(QObject *obj, QEvent *event);
+
+signals:
+
+public slots:
+    void slotGeometryChanged();
+
+private:
+    QWidget *m_pMainWindow = nullptr;
+};
+
+#endif // CUSTOMGRAPHICSVIEW_H
